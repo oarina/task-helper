3b8e1a8565c14d1d8dd5098f5ba0e5c411c8b568
Arina O <arinkaheaven@gmail.com>
2024-01-22 10:32:39 +0000

    Fix secret key var typo

--- taskhelper/settings.py
+++ taskhelper/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-oarina-taskhelper-018op5gzcw9.ws-eu107.gitpod.io','.herokuapp.com']
 
